ad7f24807f5440f9c0ae7e1cd8621aa9eb76b8ef
KkA1ZeN <daniilmanuylov239@gmail.com>
2023-10-13 20:01:17 +0300

    created function for selecting rooms
    script2.js: new eventlistener with function for selecting rooms
    
    style2.css: new styles for selected room

---
+++ script2.js
@@ -0,0 +1,29 @@
+
+ // Ссылка на главный SVG
+ const svgElement = document.getElementById('Слой_1');
+
+// Обработчик событий SVG файла для реагирования комнат на нажатие
+svgElement.addEventListener('click', (event) => {
+   // Устанавливаем ссылку на ближайшего родителя нажатого элемента с id = room... (у нас room висит над тремя дочерними полигонами, поэтому родитель)
+   const roomElement = event.target.closest('[id^="room"]');
+
+   if (roomElement) {
+     // Если такой элемент нашелся, то проверяем, есть ли сейчас уже есть активная комната, то проверяем, что это не она же самаи в противном случае удаляем у нее active
+     const activeElement = document.querySelector('[id^="room"].active');
+     if (activeElement && activeElement !== roomElement) {
+       activeElement.classList.remove('active');
+     }
+
+     // Добавляем active к новой комнате
+     roomElement.classList.add('active');
+   } else {
+     // Если кликнули не по комнате, то находим комнату с классом active и удаляем его, тем самым, выключая выделение комнаты
+     const activeElement = document.querySelector('[id^="room"].active');
+     if (activeElement) {
+       activeElement.classList.remove('active');
+     }
+   }
+ });
+
+
+
