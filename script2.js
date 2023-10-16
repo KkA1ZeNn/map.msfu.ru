7f6d5825fb398fcafdd38f21ce2175fea0c9c8c2
KkA1ZeN <daniilmanuylov239@gmail.com>
2023-10-17 00:23:58 +0300

    refactoring of cooments and formating

--- script2.js
+++ script2.js
@@ -10,10 +10,10 @@ const infoDiv = document.getElementById('discription');
 //парсим JSON
 http.open('get', './map/bmstuJson.json', true);
 http.send();
-http.onload = function(){
-  if (this.readyState === 4 && this.status === 200) {
+http.onload = function () {
+   if (this.readyState === 4 && this.status === 200) {
       parsedJson = JSON.parse(this.responseText);
-  }
+   }
 };
 
 // Обработчик событий SVG файла для реагирования комнат на нажатие
@@ -27,23 +27,21 @@ svgElement.addEventListener('click', (event) => {
       const roomId = roomElement.getAttribute('id');
       const allRooms = parsedJson.levels[0].locations;
 
-     // Если кликнули по комнате, то проверяем, есть ли сейчас уже есть активная комната, также проверяем, что это не она же сама и в противном случае удаляем у нее active
-     const activeElement = document.querySelector('[id^="room"].active');
-     if (activeElement && activeElement !== roomElement) {
-       activeElement.classList.remove('active');
-     }
+      // Если кликнули по комнате, то проверяем, есть ли сейчас уже есть активная комната, также проверяем, что это не она же сама и в противном случае удаляем у нее active
+      const activeElement = document.querySelector('[id^="room"].active');
+      if (activeElement && activeElement !== roomElement) {
+         activeElement.classList.remove('active');
+      }
 
-     // Добавляем active к новой комнате
-     roomElement.classList.add('active');
+      // Делаем комнату активной и включаем окно с описанием
+      roomElement.classList.add('active');
+      infoDiv.style.display = "flex";
 
-     //включаем наше окно с информацией
-     infoDiv.style.display = "flex";
-
-     //пробегаемся циклом по массиву с комнатами, когда находим нужную нам, берем из нее информацию для описания и вставляем его в div элемент
+      //пробегаемся циклом по массиву с комнатами, когда находим нужную нам, берем из нее информацию для описания и вставляем его в div элемент
       for (let i = 0; i < allRooms.length; ++i) {
          if (allRooms[i].id === roomId) {
-            infoDiv.innerHTML = 
-            `<h4>${allRooms[i].title}</h4>
+            infoDiv.innerHTML =
+               `<h4>${allRooms[i].title}</h4>
             <p>${allRooms[i].about}</p>`;
 
             //функция для считывания размера элемента и перемещения окна, но пока работает КРИВО
@@ -55,17 +53,17 @@ svgElement.addEventListener('click', (event) => {
       }
 
    } else {
-     // Если кликнули не по комнате, то находим комнату с классом active и удаляем его, тем самым, выключая выделение комнаты, также скрываем всплывающее окно с описанием и ставим дефолтное описание, потому что есть баг, когда после комнаты с описанием, тыкаешь на ту, где его нет, она показывает описание из предыдущей комнаты
-     const activeElement = document.querySelector('[id^="room"].active');
-     if (activeElement) {
-       activeElement.classList.remove('active');
-       infoDiv.style.display = "none";
-       infoDiv.innerHTML = 
-       `<h4>${'-'}</h4>
+      // Если кликнули не по комнате, то находим комнату с классом active и удаляем его, тем самым, выключая выделение комнаты, также скрываем всплывающее окно с описанием и ставим дефолтное описание, потому что есть баг, когда после комнаты с описанием, тыкаешь на ту, где его нет, она показывает описание из предыдущей комнаты
+      const activeElement = document.querySelector('[id^="room"].active');
+      if (activeElement) {
+         activeElement.classList.remove('active');
+         infoDiv.style.display = "none";
+         infoDiv.innerHTML =
+            `<h4>${'-'}</h4>
         <p>${'-'}</p>`;
-     }
+      }
    }
- });
+});
 
 
 
