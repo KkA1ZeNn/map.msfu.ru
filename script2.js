d30acffa0db50e678b2a1ce06f82fc3e6c7a2c55
KkA1ZeN <daniilmanuylov239@gmail.com>
2023-10-17 17:02:53 +0300

    added a function for searching a room
    its not final variation of this funtion, but the main idea was to check how it works

--- script2.js
+++ script2.js
@@ -24,23 +24,27 @@ http.onload = function () {
 
 // Обработчик событий SVG файла для реагирования комнат на нажатие
 svgElement.addEventListener('click', (event) => {
-
    // Устанавливаем ссылку на ближайшего родителя нажатого элемента с id = room... (у нас room висит на элементе с тремя дочерними полигонами, поэтому родитель)
    const roomElement = event.target.closest('[id^="room"]');
+   console.log(roomElement)
+   SelectRoom(roomElement);
+});
 
-   if (roomElement) {
+// Функция, которая отвечает за реакцию комнаты на выбор этой комнаты (нажатие или поиск)
+function SelectRoom(currentRoom) {
+   if (currentRoom) {
       //roomId-хранит айди комнаты для дальнейшего поиска в json; roomHasInfo хранит в себе статус того, нашлась информация о комнате или нет, если нет, то мы закрываем окно
-      const roomId = roomElement.getAttribute('id');
+      const roomId = currentRoom.getAttribute('id');
       let roomHasInfo = false;
 
       // Если кликнули по комнате, то проверяем, есть ли сейчас уже есть активная комната, также проверяем, что это не она же сама и в противном случае удаляем у нее active
       const activeElement = document.querySelector('[id^="room"].active');
-      if (activeElement && activeElement !== roomElement) {
+      if (activeElement && activeElement !== currentRoom) {
          activeElement.classList.remove('active');
       }
 
       // Делаем комнату активной и включаем окно с описанием
-      roomElement.classList.add('active');
+      currentRoom.classList.add('active');
       infoDiv.style.display = "flex";
 
       //пробегаемся циклом по массиву с комнатами, когда находим нужную нам, берем из нее информацию для описания и вставляем его в div элемент, если информации не нашлось, скрываем окно
@@ -52,7 +56,7 @@ svgElement.addEventListener('click', (event) => {
                <p>${allRooms[i].about}</p>`;
 
             //функция для считывания размера элемента и перемещения окна, но пока работает КРИВО
-            const roomRect = roomElement.getBoundingClientRect();
+            const roomRect = currentRoom.getBoundingClientRect();
             infoDiv.style.position = 'absolute';
             infoDiv.style.top = `${roomRect.top - 20}px`;
             infoDiv.style.left = `${roomRect.left - 120}px`;
@@ -62,7 +66,7 @@ svgElement.addEventListener('click', (event) => {
       if (!roomHasInfo) {
          infoDiv.style.display = "none";
       }
-      
+
 
    } else {
       // Если кликнули не по комнате, то находим комнату с классом active и удаляем его, тем самым, выключая выделение комнаты, также скрываем всплывающее окно с описанием и ставим дефолтное описание, потому что есть баг, когда после комнаты с описанием, тыкаешь на ту, где его нет, она показывает описание из предыдущей комнаты
@@ -72,9 +76,24 @@ svgElement.addEventListener('click', (event) => {
          infoDiv.style.display = "none";
       }
    }
-});
+};
 
+// Функция, которая отвечает за поиск комнаты через поле ввода
+function searchRoom() {
+   // stringId - хранит конечный вариант Id для последующей передачи в функцию SelectRoom(); inputOfSearch - хранит в себе текущее значение строки поиска
+   let stringId;
+   const inputOfSearch = document.getElementById('roomNumberInput').value;
 
+   if (inputOfSearch === "Актовый зал") {
+      stringId = "room-132_1_";
+   }
+   else {
+      stringId = "room-" + inputOfSearch + "_1_";
+   }
+   
+   const roomElement = document.getElementById(stringId);
+   SelectRoom(roomElement);
+}
 
 
 
