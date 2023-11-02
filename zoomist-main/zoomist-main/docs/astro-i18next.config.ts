c220ef81db343836939f52ab89e1bc6ffb734a80
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-11-02 13:45:05 +0300

    added zoomist js

---
+++ zoomist-main/zoomist-main/docs/astro-i18next.config.ts
@@ -0,0 +1,25 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+
+export type I18nLocales = 'en' | 'tw'
+
+export default {
+  defaultLocale: 'en',
+  locales: ['en', 'tw'],
+  namespaces: ['common', 'index'],
+  defaultNamespace: 'common',
+  meta: {
+    en: {
+      language: 'en',
+      name: 'English'
+    },
+    tw: {
+      language: 'zh-TW',
+      name: '繁體中文'
+    }
+  }
+  // routes: {
+  //   tw: {
+  //     '[...docs]': 'index'
+  //   }
+  // }
+}
---
+++ zoomist-main/zoomist-main/docs/routes.ts
@@ -0,0 +1,77 @@
+import { t } from 'i18next'
+
+export interface Route {
+  name: string
+  path: string | null
+  trans?: string
+  children?: Route[]
+}
+
+export type Routes = Route[]
+
+export const docsRoutes: Routes = [
+  {
+    name: 'getting-started',
+    path: '/getting-started',
+    trans: 'docNav.getting-started',
+    children: [
+      {
+        name: 'installation',
+        path: '/installation',
+        trans: 'docNav.installation'
+      },
+      {
+        name: 'basic-usage',
+        path: '/basic-usage',
+        trans: 'docNav.basic-usage'
+      }
+    ]
+  },
+  {
+    name: 'guides',
+    path: '/guides',
+    trans: 'docNav.guides',
+    children: [
+      {
+        name: 'parameters-options',
+        path: '/parameters-options',
+        trans: 'docNav.parameters-options'
+      },
+      {
+        name: 'methods-properties',
+        path: '/methods-properties',
+        trans: 'docNav.methods-properties'
+      },
+      {
+        name: 'events',
+        path: '/events',
+        trans: 'docNav.events'
+      },
+      {
+        name: 'styles',
+        path: '/styles',
+        trans: 'docNav.styles'
+      },
+      {
+        name: 'using-typescript',
+        path: '/using-typescript',
+        trans: 'docNav.using-typescript'
+      }
+    ]
+  }
+]
+
+export const flatDocsRoutes: Route[] = docsRoutes
+  .map((route) => {
+    return route.children && route.children.length
+      ? route.children.map((item) => ({
+          ...item,
+          path: `${route.path}${item.path}`
+        }))
+      : { ...route }
+  })
+  .flat()
+
+export const useRouteTrans = (route: Route): string => {
+  return route.trans ? t(route.trans) : route.name
+}
