61969508c1ed88c5530d5d393ce072ca6466bce9
KkA1ZeN <daniilmanuylov239@gmail.com>
2024-02-21 12:16:32 +0300

    fixed bug with importing panzoom

--- gulpSettings/task/js.js
+++ gulpSettings/task/js.js
@@ -3,9 +3,14 @@ import gulpPlumber from "gulp-plumber";
 import babel from "gulp-babel";
 import webpack from "webpack-stream";
 
+const scriptFiles = [
+   './src/js/modules/panzoom.min.js',
+   './src/js/main.js',
+]
+
 // processing JS here you can add your new plugins in stream
 const js = () => {
-   return $.gulp.src($.path.js.src, { sourcemaps: $.pluginSettings.isDev })
+   return $.gulp.src(scriptFiles, { sourcemaps: $.pluginSettings.isDev })
    .pipe(gulpPlumber())
    .pipe(babel())
    .pipe(webpack($.pluginSettings.webpack))
